29a4fa7d7ee80fa69dd1899c5156e01ac5ad1537
joy-ahmed <esthersboydd7806@gmail.com>
2022-03-04 10:11:58 +0600

    new update with packeges

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 4242;
+const Book = require('./models/bookModel');
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    // eslint-disable-next-line array-callback-return
+    Book.find((_query, err, books) => {
+      if (err) res.send(err);
+      res.json(books);
+    });
+  });
+
+app.use('/api', bookRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
